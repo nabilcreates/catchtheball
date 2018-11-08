da756a6514e5cfef617c70a93b6695dbb2f31f57
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-08 10:21:21 +0800

    Added difficulty text (and repawn max width and height but minus 200)

--- main.js
+++ main.js
@@ -12,10 +12,12 @@ var cursor = {
     d: 10,
 }
 
-var speed = 1;
+// SPEED IS THE SAME AS DIFFICULTY
+var speed = 2;
+var level = 'BEGINNER'
 
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
@@ -24,10 +26,13 @@ function draw() {
     // DISPLAY SCORE
     fill(255)
     text('Score: ' + score, 10, 30);
+    text('Difficulty: ' + level, 10, 50);
+
 
     // RUN FUNCTIONS (BELOW)
     checkForIntersect()
     checkOutOfCanvas()
+    checkLevel()
 
     // MAKE E.X AND E.Y RANDOM (SO IT MOVES RANDOMLY)
     e.x += random(0, speed)
@@ -54,9 +59,9 @@ function checkForIntersect() {
         // ADD THE SCORE
         score++
 
-        // E.X AND E.Y FO TO RANDOM LOCATION
-        e.x = random(width)
-        e.y = random(height)
+        // E.X AND E.Y FO TO RANDOM LOCATION (Minus 200 because sometimes it will spawn at the end of the canvas)
+        e.x = random(width - 200)
+        e.y = random(height - 200)
     }
 }
 
@@ -68,11 +73,44 @@ function checkOutOfCanvas() {
         // THEN CONSOLE LOG OUT
         console.log('out')
 
-        // E.X AND E.Y GO TO RANDOM LOCATION
-        e.x = random(width)
-        e.y = random(height)
+        // E.X AND E.Y GO TO RANDOM LOCATION (Minus 200 because sometimes it will spawn at the end of the canvas)
+        e.x = random(width - 200)
+        e.y = random(height - 200)
 
         // SCORE = 0
         score = 0;
     }
+}
+
+// INCREASES THE SPEED (DIFICULTY ACCORDING TO SCORE)
+function checkLevel(){
+
+    switch(score){
+
+        case 0:
+        speed = 1;
+        level = "BEGINNER"
+        break;
+        
+        case 5:
+        speed = 2;
+        level = "MMHMM..."
+        break;
+
+        case 7:
+        speed = 3;
+        level = "shiiiiiiit..."
+        break;
+        
+        case 10:
+        speed = 5;
+        level = "DAAAAAAAAAAAAAAAMN"
+        break;
+
+        case 15:
+        speed = 9;
+        level = "HOLDING UP GOOD?"
+        break;
+    }
+    
 }
